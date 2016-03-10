c771c79493db621c73d078f948669a5485d7f237
Anjack <gary.reif@gmail.com>
2016-03-10 02:57:49 +0000

    Moved response handling to discreet function within httpServer.js. Updated httpRequestRouter.js to use this function.

--- httpRequestRouter.js
+++ httpRequestRouter.js
@@ -1,9 +1,11 @@
+var httpServer = require("./httpServer");
 
-
-function routeRequest(pathname) {
+function routeRequest(pathname, response) {
 	// DEBUG START
 	console.log("Routing request to: " + pathname)
 	// DEBUG END
+	
+	httpServer.sendResponse(response);
 }
 
 exports.routeRequest = routeRequest;
--- httpServer.js
+++ httpServer.js
@@ -4,19 +4,26 @@ var url = require("url");
 function startServer(route) {
 	function onRequest(request, response) {
 		var pathname = url.parse(request.url).pathname;
+		console.log("Request received: " + pathname);
 		// DEBUG START
 		console.log("Request " + request.method + ": " + pathname + ".\nHeaders:\n", request.headers, "\nTrailers:\n", request.trailers);
 		// DEBUG END
+        
+        if (request.method = 'GET') {
+            route(pathname, response);
+        };
 		
-		route(pathname);
-		
-		response.writeHead(200, {"Content-Type": "text/plain"});
-		response.write("Hello world!");
-		response.end();
 	}
 
 	http.createServer(onRequest).listen(8080);
 	console.log("HTTP server running.");
 }
 
+function sendResponse(response) {
+	response.writeHead(200, {"Content-Type": "text/plain"});
+	response.write("Hello dude!");
+	response.end();
+}
+
 exports.startServer = startServer;
+exports.sendResponse = sendResponse;
